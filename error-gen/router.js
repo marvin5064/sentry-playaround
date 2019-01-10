00daf769b77608aa1bd49a0c7f961ccb1e51af5f
marvin cai <marvin5064@gmail.com>
2019-01-11 00:41:30 +0800

    5 level of loggin

--- error-gen/router.js
+++ error-gen/router.js
@@ -1,6 +1,27 @@
+const Sentry = require('@sentry/node');
+
 const Router = function(app) {
   app.get('/info/:message', (req, res) => {
     res.send(`Info: ${req.params.message}`);
+  });
+  app.get('/capture-error/:message', (req, res) => {
+    Sentry.captureException('capture-error: ${req.params.message}');
+    res.send(`capture-error: ${req.params.message}`);
+  });
+  app.get('/capture-message/:level', (req, res) => {
+    // Available Logging Levels:
+    // fatal
+    // error
+    // warning
+    // info
+    // debug
+    const msg = `capture-message with level ${req.params.level}`;
+    Sentry.withScope(scope => {
+      scope.setLevel(req.params.level);
+      Sentry.captureMessage(msg);
+    });
+    console.log(msg);
+    res.send(msg);
   });
   app.get('/error/:message', (req, res) => {
     throw new Error(req.params.message);
